ffce58bdcd73182e14794f76bf2279e750ae5ee7
Silverz21 <z44santos@gmail.com>
2023-04-10 20:19:47 -0600

    Se extrae y grafica la base de datos

--- Tarea_3.py
+++ Tarea_3.py
@@ -5,3 +5,15 @@ Created on Mon Apr 10 18:39:20 2023
 @author: Lenovo
 """
 
+import pandas as pd
+import datetime
+
+paste = pd.read_csv('Puebla_pos.csv')
+
+Casos = pd.DataFrame(paste)
+
+Casos.FECHA_SINTOMAS = pd.to_datetime(Casos.FECHA_SINTOMAS)
+
+Casos = Casos.set_index("FECHA_SINTOMAS")
+
+Casos["positivo"].resample("W").sum().plot()
